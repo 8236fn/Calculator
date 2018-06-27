5df3cc1e6ea46f238bfe63b9ad900d0104ddddca
8236fn <40617124+8236fn@users.noreply.github.com>
2018-06-27 13:04:27 +0800

    20170627-4  java change

--- app/src/main/java/com/blogspot/tiaotone/calculator/MainActivity.java
+++ app/src/main/java/com/blogspot/tiaotone/calculator/MainActivity.java
@@ -2,14 +2,87 @@ package com.blogspot.tiaotone.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView Ans;
+    private Button btnBack,btnClear,btnPoint,btnEquals;
+    private Button btnAdd,btnSubtract,btnMultiply,btnDivided;
+    private Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    private double theValue = 0, operand1 = 0, operand2 = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
 
+        Ans = (TextView)findViewById(R.id.display);
+        btnBack = (Button)findViewById(R.id.btnBack);
+        btnClear = (Button)findViewById(R.id.btnClear);
+        btnPoint = (Button)findViewById(R.id.btnPoint);
+        btnEquals = (Button)findViewById(R.id.btnEquals);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnSubtract = (Button)findViewById(R.id.btnSubtract);
+        btnMultiply = (Button)findViewById(R.id.btnMultiplay);
+        btnDivided = (Button)findViewById(R.id.btnDivided);
+        btn0 = (Button)findViewById(R.id.btn0);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        btn4 = (Button)findViewById(R.id.btn4);
+        btn5 = (Button)findViewById(R.id.btn5);
+        btn6 = (Button)findViewById(R.id.btn6);
+        btn7 = (Button)findViewById(R.id.btn7);
+        btn8 = (Button)findViewById(R.id.btn8);
+        btn9 = (Button)findViewById(R.id.btn9);
+
+        btnPoint.setText(".");
+        btnEquals.setText("=");
+        btnAdd.setText("+");
+        btnSubtract.setText("-");
+        btnMultiply.setText("*");
+        btnDivided.setText("/");
+        btn0.setText("0");
+        btn1.setText("1");
+        btn2.setText("2");
+        btn3.setText("3");
+        btn4.setText("4");
+        btn5.setText("5");
+        btn6.setText("6");
+        btn7.setText("7");
+        btn8.setText("8");
+        btn9.setText("9");
+
+        btnBack.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+        btnPoint.setOnClickListener(this);
+        btnEquals.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+        btnSubtract.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnDivided.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
