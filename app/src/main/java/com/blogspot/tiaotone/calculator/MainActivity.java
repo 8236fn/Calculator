b8dd1f9f8b9081aebcbdcf4805553faf339e19b5
8236fn <40617124+8236fn@users.noreply.github.com>
2018-06-28 12:13:02 +0800

    20180628 final

--- app/src/main/java/com/blogspot/tiaotone/calculator/MainActivity.java
+++ app/src/main/java/com/blogspot/tiaotone/calculator/MainActivity.java
@@ -4,25 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView display;
-    private Button btnBack,btnClear,btnPoint,btnEquals;
+    private Button btnClear,btnEquals;
     private Button btnAdd,btnSubtract,btnMultiply,btnDivided;
     private Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
-    private double theValue = 0, operand1 = 0, operand2 = 0 ;
+    private String result, tmp, operator ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
-
+        initControl();
+        initControlListener();
+    }
+    private void initControl() {
         display = (TextView)findViewById(R.id.display);
-        btnBack = (Button)findViewById(R.id.btnBack);
         btnClear = (Button)findViewById(R.id.btnClear);
-        btnPoint = (Button)findViewById(R.id.btnPoint);
         btnEquals = (Button)findViewById(R.id.btnEquals);
         btnAdd = (Button)findViewById(R.id.btnAdd);
         btnSubtract = (Button)findViewById(R.id.btnSubtract);
@@ -38,59 +38,143 @@ public class MainActivity extends AppCompatActivity {
         btn7 = (Button)findViewById(R.id.btn7);
         btn8 = (Button)findViewById(R.id.btn8);
         btn9 = (Button)findViewById(R.id.btn9);
-
-        btnPoint.setText(".");
-        btnEquals.setText("=");
-        btnAdd.setText("+");
-        btnSubtract.setText("-");
-        btnMultiply.setText("*");
-        btnDivided.setText("/");
-        btn0.setText("0");
-        btn1.setText("1");
-        btn2.setText("2");
-        btn3.setText("3");
-        btn4.setText("4");
-        btn5.setText("5");
-        btn6.setText("6");
-        btn7.setText("7");
-        btn8.setText("8");
-        btn9.setText("9");
-
-        btnBack.setOnClickListener(onClickListener);
-        btnClear.setOnClickListener(onClickListener);
-        btnPoint.setOnClickListener(onClickListener);
-        btnEquals.setOnClickListener(onClickListener);
-        btnAdd.setOnClickListener(onClickListener);
-        btnSubtract.setOnClickListener(onClickListener);
-        btnMultiply.setOnClickListener(onClickListener);
-        btnDivided.setOnClickListener(onClickListener);
-        btn0.setOnClickListener(onClickListener);
-        btn1.setOnClickListener(onClickListener);
-        btn2.setOnClickListener(onClickListener);
-        btn3.setOnClickListener(onClickListener);
-        btn4.setOnClickListener(onClickListener);
-        btn5.setOnClickListener(onClickListener);
-        btn6.setOnClickListener(onClickListener);
-        btn7.setOnClickListener(onClickListener);
-        btn8.setOnClickListener(onClickListener);
-        btn9.setOnClickListener(onClickListener);
     }
-    private View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            int btnId = ((Button)view).getId();
-            String displayStr = display.getText().toString();
-            switch (btnId){
-                case R.id.btnBack:
-                    if (displayStr.length()>1)
-                        display.setText(displayStr.substring(0,displayStr.length()-1));
-                    else if (displayStr.length()==1){
-
-                    }
+    private void initControlListener() {
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClearButtonClicked();
             }
-
+        });
+        btnEquals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEqualButtonClicked();
+            }
+        });
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("+");
+            }
+        });
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("-");
+            }
+        });
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("*");
+            }
+        });
+        btnDivided.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("/");
+            }
+        });
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("0");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("9");
+            }
+        });
+    }
+    private void onNumberButtonClicked(String s) {
+        result = display.getText().toString();
+        result = result + s;
+        display.setText(result);
+    }
+    private void onOperatorButtonClicked(String s) {
+        tmp = display.getText().toString();
+        onClearButtonClicked();
+        this.operator = s;
+    }
+    private void onEqualButtonClicked() {
+        double res = 0;
+        try {
+            double number = Double.valueOf(tmp);
+            double number2 = Double.valueOf(display.getText().toString());
+            switch (operator) {
+                case "+":
+                    res = number + number2;
+                    break;
+                case "-":
+                    res = number - number2;
+                    break;
+                case "*":
+                    res = number * number2;
+                    break;
+                case "/":
+                    res = number / number2;
+                    break;
+            }
+            result = Double.toString(res);
+            display.setText(result);
         }
-    };
-
-
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void onClearButtonClicked() {
+        result = "";
+        display.setText(result);
+    }
 }
